195ca5879a819389c636bc66e5913d3dcac03777
skyoo1226 <you1226@naver.com>
2022-02-26 15:35:45 +0900

    리스트뷰 공부 시작  - data  - kotle / class 만들기  - 멤버 변수 지정  - 클래스 생성  - 리스트값 넘기도록  - 데이터9개 추가

--- app/src/main/java/com/knta/listview_20220226/MainActivity.kt
+++ app/src/main/java/com/knta/listview_20220226/MainActivity.kt
@@ -2,6 +2,7 @@ package com.knta.listview_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.knta.listview_20220226.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mStudentList.
+        mStudentList.add( StudentData("조경진", 1988, "서울시 동대문구")  )
+        mStudentList.add( StudentData("유석균", 1970, "서울시 강북")  )
+        mStudentList.add( StudentData("권효영", 1981, "서울시 중구")  )
+        mStudentList.add( StudentData("김한균", 1991, "서울시 송파구")  )
+        mStudentList.add( StudentData("최민서", 1990, "서울시 천호구")  )
+        mStudentList.add( StudentData("최다영", 1994, "서울시 의정부")  )
+        mStudentList.add( StudentData("김정환", 1985, "서울시 서대구")  )
+        mStudentList.add( StudentData("권효영", 1984, "서울시 마포구")  )
+        mStudentList.add( StudentData("이현종", 1989, "서울시 아현구")  )
+
+
 
 
     }
