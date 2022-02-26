0d5b20a97f433b8021ba4a6ec51b728bcb0a29e8
skyoo1226 <you1226@naver.com>
2022-02-26 15:25:18 +0900

    리스트뷰 공부 시작  - data  - kotle / class 만들기  - 멤버 변수 지정  - 클래스 생성  - 리스트값 넘기도록

--- app/src/main/java/com/knta/listview_20220226/Adapter/StudentAdapter.kt
+++ app/src/main/java/com/knta/listview_20220226/Adapter/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.knta.listview_20220226.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.knta.listview_20220226.R
 import com.knta.listview_20220226.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,25 @@ class StudentAdapter(
     val resId : Int,
     val mList : ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList)  {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item,null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/knta/listview_20220226/MainActivity.kt
+++ app/src/main/java/com/knta/listview_20220226/MainActivity.kt
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList : AppCompatActivity() {
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.
 
 
     }
